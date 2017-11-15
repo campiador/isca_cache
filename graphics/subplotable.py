2bd7d59e7d4ad9e08fc044faeca3f22fad61bd24
campiador <behnam.heydarshahi@gmail.com>
2017-11-15 14:35:24 -0500

    initial plotting

---
+++ graphics/subplotable.py
@@ -0,0 +1,11 @@
+#  subplotable.py
+#  by Behnam Heydarshahi, October 2017
+#
+#  This class models data needed for a single sub plot
+
+class SubPlotable:
+    def __init__(self, label, x_values, y_values, y_std_error_values):
+        self.label = label
+        self.x_values = x_values
+        self.y_values = y_values
+        self.y_std_err_values = y_std_error_values
